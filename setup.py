543612e5b5059e1f6316098e401779a5c98a5079
Alex <alejandro@gafa.mx>
2018-02-23 18:34:41 +0100

    Update config files

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 setup(
     name='onesignalapi',
-    packages=find_packages(),  # this must be the same as the name above
-    version='0.3',
+    packages=find_packages(),
+    version='0.4',
     description='One Signal API SDK https://onesignal.com',
     author='Alejandro Romero',
     author_email='ealejandrorg@gmail.com',
-    url='https://github.com/alexromer0/onesignal-api.git',  # use the URL to the github repo
-    download_url='https://github.com/alexromer0/onesignal-api.git/tarball/0.3',
+    url='https://github.com/alexromer0/onesignal-api.git',
+    download_url='https://github.com/alexromer0/onesignal-api.git/tarball/0.4',
     keywords=['OneSignal', 'Notifications', 'Push'],
     license='MIT',
     classifiers=[
@@ -18,5 +18,5 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',], install_requires=['pytz']
+        'Programming Language :: Python :: 2.7', ], install_requires=['pytz']
 )
